455f1f5993ea51cb07c2bd008336cabfd5541594
Nguyễn Thành Luân <luannguyen@prep.vn>
2024-09-05 19:55:06 +0700

    Fix: Remove jsp config in properties file

--- src/main/java/luan/com/niit/springboot10/controllers/HomeController.java
+++ src/main/java/luan/com/niit/springboot10/controllers/HomeController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("homeController")
+@Controller()
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+
+    @GetMapping
     public String home() {
         return "index";
     }
